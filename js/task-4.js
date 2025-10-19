72b951f42095bd51c7153886b3d79ac8b1e1268b
Prigoda <sergey.prigoda@gmail.com>
2025-10-19 23:26:23 +0300

    end task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: email,
+    password: password,
+  };
+
+  console.log(formData);
+
+  form.reset();
+}
